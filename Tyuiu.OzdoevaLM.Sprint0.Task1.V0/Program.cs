1879110e39ab061fcd70e454e5ac5cdfcb7ae0ca
OzdoevaLeiila <vnches@mail.ru>
2025-09-17 19:21:35 +0500

    Task1

---
+++ Tyuiu.OzdoevaLM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.OzdoevaLM.Sprint0.Task1.V0
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Лейла";
+            int age = 19;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
