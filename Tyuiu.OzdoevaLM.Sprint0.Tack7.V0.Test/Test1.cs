50e627917177b635c2e92b48975c884325229781
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-01 20:38:48 +0500

    Task7

---
+++ Tyuiu.OzdoevaLM.Sprint0.Tack7.V0.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.OzdoevaLM.Sprint0.Tack7.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
